96e026f8bcb6204a3877ef81752f817bee5c5ab7
Duong <duongkc4444@gmail.com>
2025-05-23 19:02:04 +0700

    ex13.9

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,4 @@
 const router = require("express").Router();
-// Add support for users to the application.
-// In addition to ID, users have the following fields:
-// name (string, must not be empty)
-// username (string, must not be empty)
-// do not prevent Sequelize from creating timestamps created_at and updated_at for users
-
-// All users can have the same password as the material.
-// Implement the following routes
-// POST api/users (adding a new user)
-// GET api/users (listing all users)
-// PUT api/users/:username (changing a username, keep in mind that the parameter is not id but username)
-// Make sure that the timestamps created_at and updated_at automatically set by Sequelize work correctly
-//  when creating a new user and changing a username.
 const { User } = require("../models");
 const { Blog } = require("../models");
 router.get("/", async (req, res) => {
@@ -25,12 +12,8 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  try {
     const user = await User.create(req.body);
     res.json(user);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
 });
 
 router.get("/:username", async (req, res) => {
--- util/middleware.js
+++ util/middleware.js
@@ -1,23 +1,26 @@
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-const errorHandler = (error, request, response, next) => {
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (
-    ['SequelizeValidationError', 'ValidationError', 'SyntaxError'].includes(
-      error.name
-    )
-  ) {
-    return response.status(400).json({ error: error.message })
+const errorHandler = (error, request, response, next) => { 
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
+  } else if (error.name === 'SequelizeDatabaseError') {
+    return response.status(500).json({ error: error.message });
   } else if (error.name === 'TypeError') {
-    return response.status(500).json({ error: error.message })
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === 'SequelizeValidationError') {
+    return response.status(400).json({ error: "invalid username" });
   }
-  next(error)
-}
+
+  next(error);
+};
+
 
 module.exports = {
   unknownEndpoint,
   errorHandler,
-}
+};
