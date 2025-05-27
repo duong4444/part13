f92aeafc41595e540fabc3b51e83665b5c5826c2
Duong <duongkc4444@gmail.com>
2025-05-27 17:41:25 +0700

    ex13.23

--- controllers/users.js
+++ controllers/users.js
@@ -21,6 +21,11 @@ router.post("/", async (req, res) => {
 
 
 router.get('/:id', async (req, res) => {
+  const where = {}
+  if(req.query.read){
+    where.read = req.query.read === 'true'
+  }
+
   const user = await User.findByPk(req.params.id, {
     attributes: { exclude: ['createdAt', 'updatedAt', 'id'] },
     include: [
@@ -35,6 +40,7 @@ router.get('/:id', async (req, res) => {
           attributes: [
             'read','id'
           ],
+          where
         },
       },
     ],
